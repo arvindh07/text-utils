5c216efce799f419f88cda3ab3f969a0154b0e3c
arvindh07 <arvindhganesh11@gmail.com>
2022-06-16 11:19:33 +0530

    Updates

---
+++ precache-manifest.4e5a95f80e8af2966502d7b25ac8af54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c81ffa6ebaee264b13ea",
+    "url": "/text-utils/static/css/main.def4e6cd.chunk.css"
+  },
+  {
+    "revision": "c81ffa6ebaee264b13ea",
+    "url": "/text-utils/static/js/main.c81ffa6e.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/text-utils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/text-utils/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "01d243e167ff31ad444b",
+    "url": "/text-utils/static/js/runtime~main.01d243e1.js"
+  },
+  {
+    "revision": "5e9d6ffb2352fa840359ada95f1327d4",
+    "url": "/text-utils/index.html"
+  }
+];
